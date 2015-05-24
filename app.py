6c085f669e08efd9991d59da3aad1ff43a3c97f7
ono <100blendy@gmail.com>
2015-05-25 05:08:08 +0900

    修正

--- app.py
+++ app.py
@@ -1,19 +1,20 @@
 # coding: utf-8
-import os, re
+import os, re, json
 
 path = os.getcwd()
 base = os.path.dirname(os.path.abspath(__file__))
-ext = '.styl'
+extList = ['.styl', '.scss', '.sass', '.less']
 
 pattern = re.compile('/\*([^/]|[^*]/)*\*/', re.DOTALL)
 pattern2 = re.compile('//.*')
 
 
 # ファイル内からimportを探す
-def getImport(files):
+def getImport(files, dict):
     id = files[0]
     root = files[1]
     name = files[2]
+    ext = files[3]
     importList = []
     i = 0
 
@@ -29,13 +30,18 @@ def getImport(files):
 
     # @importを探す
     while True:
+        quotes = '"'
         filepath = ''
         filename = ''
         i = styl.find('@import', i)
         if i <= 0:
             break
-        posS = styl.find('"', i)
-        posE = styl.find('"', posS + 1)
+        posS = styl.find(quotes, i, i + 10)
+        # ダブルクォーテーションを使ってなかったらシングルに変更
+        if posS <= 0:
+            quotes = "'"
+            posS = styl.find(quotes, i)
+        posE = styl.find(quotes, posS + 1)
         pathname = styl[posS+1:posE]
         boundary = pathname.rfind('/')
         if boundary == -1:
@@ -45,41 +51,54 @@ def getImport(files):
             filepath = pathname[:boundary+1]
             filename = pathname[boundary+1:]
 
-        importList.append(os.path.abspath(filepath).replace(base, '') + '/' + filename + ext)
+        key = os.path.abspath(filepath).replace(base, '') + '/' + filename + ext
+
+        if key in dict:
+            dict[key].append(id.replace(base, ''))
+        else:
+            dict[key] = [(id.replace(base, ''))]
         i = posE
-    return [id.replace(base, ''), importList]
+    return dict
 
 # 訪問リスト作成
 def getFileList():
     fileList = []
     for root, dirs, files in os.walk(path):
         for file in files:
-            if ext in file:
-                fileList.append([os.path.join(root,file), root, file])
+            for ext in extList:
+                if ext in file:
+                    fileList.append([os.path.join(root,file), root, file, ext])
+                    break
     return fileList
 
 # report作成
-def makeReport(partialList):
-    str = ''
-    for lists in partialList:
-        str += '\r\n'
-        str += '■ファイルの名前：' + lists[0] + '\r\n\r\n'
-        for li in lists[1]:
-            str += li + '\r\n'
-        str += '\r\n'
+def makeReport(dict):
+    # reportディレクトリが無かったら作って移動
+    if os.path.exists(path + '/report/') == False:
+        os.mkdir('report')
+    os.chdir(path + '/report/')
+    # str = ''
+    # for k, v in dict.items():
+    #     str += '\r\n'
+    #     str += '■パーシャル名：' + k + '\r\n\r\n'
+    #     for li in v:
+    #         str += li + '\r\n'
+    #     str += '\r\n'
 
-    os.chdir(path)
-
-    report = open('report.txt', 'w')
-    report.write(str)
+    # report = open('report.txt', 'w')
+    # report.write(str)
+    # report.close()
+    report = open('report.json', 'w')
+    report.write(json.dumps(dict, sort_keys=True, indent=4))
     report.close()
 
 def init():
-    partialList = []
+    partialDict = {}
     fileList = getFileList()
     for list in fileList:
-        partialList.append(getImport(list))
-    makeReport(partialList)
+        partialDict = getImport(list, partialDict)
+    os.chdir(path)
+    makeReport(partialDict)
 
 init()
 
